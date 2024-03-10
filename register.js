99f0c0786e5d8d43d8afacc2f10d20b221f16548
godolias15 <159550632+godolias15@users.noreply.github.com>
2024-03-10 19:49:28 +0300

    Add files via upload

---
+++ register.js
@@ -0,0 +1,7 @@
+// JavaScript for registration page (register.js)
+
+// Assuming registration is successful
+function registrationSuccess() {
+    // Redirect to home page
+    window.location.href = "index.html";
+}
